cebe04af159fce140fd6a8e05cc66fc26111c764
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-02-22 22:29:04 +0200

    fixing footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,73 +1,45 @@
 
 export default function Footer() {
   return (
-    <footer className="relative bg-blueGray-200 pt-8 pb-6 border-t border-blueGray-300">
-  <div className="container mx-auto px-4">
-    <div className="flex flex-wrap text-left lg:text-left">
-      <div className="w-full lg:w-6/12 px-4">
-        <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
-        <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-          Find us on any of these platforms, we respond 1-2 business days.
-        </h5>
-        <div className="mt-6 lg:mb-0 mb-6">
-          <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i className="fab fa-twitter"></i></button><button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i className="fab fa-facebook-square"></i></button><button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i className="fab fa-dribbble"></i></button><button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i className="fab fa-github"></i>
-          </button>
-        </div>
-      </div>
-      <div className="w-full lg:w-6/12 px-4">
-        <div className="flex flex-wrap items-top mb-6">
-          <div className="w-full lg:w-4/12 px-4 ml-auto">
-            <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
-            <ul className="list-unstyled">
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</a>
-              </li>
-            </ul>
+    <>
+    <footer className=" bg-gray-100 pt-8 border-t border-blueGray-300">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap text-left lg:text-left">
+          <div className="w-full lg:w-6/12 px-4">
+            <h4 className="text-3xl font-bold text-blueGray-700 mb-3"><span><i className="fa-solid fa-cart-shopping pr-2"></i></span>Fresh Cart</h4>
+            <p className="text-2xl font-semibold text-blueGray-700 mb-3">Best Way to Shop Online</p>
+            <div className="space-y-2">
+              <p className="font-serif"><span><i className="pr-3 fa-solid fa-location-dot"></i></span> Cairo ,Egypt</p>
+              <p className="font-serif"><span><i className="pr-3 fa-solid fa-envelope"></i>ahmedsamyalshmony1@gmail.com</span></p>
+              <p className="font-serif"><span><i className="pr-3 fa-solid fa-phone-volume"></i></span>01284682092</p>
+            </div>
           </div>
-          <div className="w-full lg:w-4/12 px-4">
-            <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-            <ul className="list-unstyled">
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
-              </li>
-              <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
-              </li>
-            </ul>
+          <div className="w-full lg:w-6/12 px-4 pt-5 lg:pt-0">
+            <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+              Find us on any of these platforms, we respond 1-2 business days.
+            </h5>
+            <div className="mt-6 lg:mb-0 mb-6 space-x-3">
+              <a target="_blank" href="https://github.com/Alashmony2" className="bg-white text-lightBlue-400 shadow-lg text-3xl font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                <i className="fab fa-github"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/in/ahmed-alashmony-90b133253/" className="bg-white text-3xl text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                <i className="fa-brands fa-linkedin"></i></a>
+                <a target="_blank" href="https://www.facebook.com/ahmedsamyalashmony/" className="bg-white  shadow-lg font-normal h-10 w-10 text-3xl items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                <i className="fa-brands fa-facebook"></i></a>
+                
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <hr className="my-6 border-blueGray-300"/>
-    <div className="flex flex-wrap items-center md:justify-between justify-center">
-      <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-        <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span><a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by
-          </a>
-          <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>
+    </footer>
+    <div className="p-10 flex-wrap items-center md:justify-between justify-center bg-gray-200">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-blueGray-500 font-semibold py-1">
+              Copyright © <span id="get-current-year">2025 </span>
+              <a className="text-blueGray-500 hover:text-blueGray-800"> Ahmed Alashmony</a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</footer>
+    </>
   )
 }
